83998afa6dcd681e9ced846a026ca650bf94ee4b
Fedor Arbuzov <FPArbuzov@edu.hse.ru>
2016-12-08 05:26:54 +0300

    Delete send_message.py

--- send_message.py
+++
@@ -1,9 +0,0 @@
-import smtplib
-
-server = smtplib.SMTP('smtp.gmail.com', 587, "arbuzovfp@gmail.com", timeout=120)
-server.starttls()
-server.login("arbuzovfp@gmail.com", "CShse2016")
-
-msg = "YOUR MESSAGE!"
-server.sendmail("arbuzovfp@gmail.com", "ArbuzovFP@yandex.ru", msg)
-server.quit()
